6952bfa4e5c3fdc8b23fa9d3a37f3f288bb3e92a
Wenjie Zhang <19354817+myxxxsquared@users.noreply.github.com>
2020-05-14 03:32:26 +0800

    almost finish

--- train.py
+++ train.py
@@ -6,6 +6,9 @@ from model import Transformer
 from preprocess import preprocessdata
 import tqdm
 
+import nltk
+from nltk.tokenize import word_tokenize
+from rouge import Rouge 
 
 def genpaddingmask(data):
     return tf.cast(data == 0, tf.float32)[:, tf.newaxis, tf.newaxis, :]
@@ -152,13 +155,15 @@ def main():
         t = tqdm.tqdm(enumerate(data.data_train))
         for batch, (en, cn) in t:
             train_step(en, cn)
-            t.desc = f"{epoch:03d}: {train_loss.result():.4f}, {train_accuracy.result():.4f}"
+            t.desc = (
+                f"{epoch:03d}: {train_loss.result():.4f}, {train_accuracy.result():.4f}"
+            )
 
         t = tqdm.tqdm(enumerate(data.data_validation))
         for batch, (en, cn) in t:
             valid_step(en, cn)
             t.desc = f"{epoch:03d}: {train_accuracy.result():.4f}"
-        print(f'Epoch {epoch:03d}: {train_accuracy.result():.4f}')
+        print(f"Epoch {epoch:03d}: {train_accuracy.result():.4f}")
 
         ckpt_save_path = ckpt_manager.save()
 
@@ -169,8 +174,6 @@ def main():
             maxtrainacc = curtrainacc
             print("Copy Best")
 
-        
-
         # print(
         #     "Epoch {} Loss {:.4f} Accuracy {:.4f}".format(
         #         epoch + 1, train_loss.result(), train_accuracy.result()
@@ -179,6 +182,7 @@ def main():
 
 
 def evaluation():
+    rouge = Rouge()
     data = preprocessdata()
     params = HyperParameters(data)
 
@@ -199,30 +203,71 @@ def evaluation():
     ckpt.restore(ckpt_manager.latest_checkpoint).expect_partial()
 
     NUM_BEAM = 4
+    SEQLEN = 20
 
-    @tf.function
     def eval_step(en, cn, idx):
         en_input = en
         input_mask = genpaddingmask(cn)
-        decode_mask = tf.maximum(
-            genpaddingmask(en_input), genlookmask(tf.shape(en_input)[1])
-        )
+        decode_mask = genlookmask(tf.shape(en_input)[1])
 
         pred, _ = model(cn, en_input, False, input_mask, decode_mask, input_mask)
+        pred = pred[:, idx]
         pred = tf.nn.softmax(pred, -1)
+
+        pred = tf.reshape(pred, (-1, NUM_BEAM, params.en_vocab_size))
         values, indices = tf.math.top_k(pred, NUM_BEAM)
         return values, indices
-
-    SEQLEN = 20
 
     total = 0
     count = 0
 
+    totalb = 0
+    totalr = 0
+    totalm = 0
+    countb = 0
+
     for batch, (en, cn) in enumerate(data.data_test):
-        en_pred = np.zeros(shape=(int(tf.shape(cn)[0]), NUM_BEAM, SEQLEN), dtype=np.int)
+        batch_size = int(tf.shape(cn)[0])
+
+        cn_origin = cn
+
+        cn = cn[:, tf.newaxis]
+        cn = tf.tile(cn, (1, NUM_BEAM, 1))
+        cn = tf.reshape(cn, (-1, SEQLEN))
+
+        finished = np.zeros(shape=(batch_size, NUM_BEAM), dtype=np.bool)
+        prob = np.ones(shape=(batch_size, NUM_BEAM), dtype=np.float)
+        en_pred = np.zeros(shape=(batch_size, NUM_BEAM, SEQLEN), dtype=np.int)
         en_pred[:, :, 0] = data.entok_begin
+
         for outlab in range(SEQLEN - 1):
-            values, indices = eval_step(tf.convert_to_tensor(en_pred[:, :-1]), cn, outlab)
+            values, indices = eval_step(
+                tf.reshape(tf.convert_to_tensor(en_pred[:, :, :-1]), (-1, SEQLEN-1)), cn, outlab
+            )
+
+            new_finished = np.logical_or(indices.numpy() == data.entok_end, np.expand_dims(finished, 1))
+            values = values.numpy()
+            values[new_finished] = 1
+            new_prob = np.expand_dims(prob, -1) * values
+            new_pred = np.tile(np.expand_dims(en_pred, -2), (1, 1, NUM_BEAM, 1))
+            new_pred[:, :, :, outlab + 1] = indices
+
+            new_prob = np.reshape(new_prob, (-1, NUM_BEAM * NUM_BEAM))
+            new_pred = np.reshape(new_pred, (-1, NUM_BEAM * NUM_BEAM, SEQLEN))
+
+            sort_prob = np.argsort(new_prob)[:, ::-1]
+            prob = np.empty(shape=prob.shape, dtype=prob.dtype)
+            en_pred = np.empty(shape=en_pred.shape, dtype=en_pred.dtype)
+            for i in range(batch_size):
+                for j in range(NUM_BEAM):
+                    en_pred[i, j] = new_pred[i, sort_prob[i, j]]
+                    prob[i, j] = new_prob[i, sort_prob[i, j]]
+
+            finished = np.logical_or(
+                finished, en_pred[:, :, outlab + 1] == data.entok_end
+            )
+
+        en_pred = en_pred[:, 0]
 
         for i, x in enumerate(en_pred):
             for j in range(SEQLEN - 1):
@@ -239,38 +284,53 @@ def evaluation():
         total += np.sum(nonpad)
         count += np.sum(eq)
 
-
-        for idx in range(10):
+        for idx in range(batch_size):
             x = en_ground[idx].numpy()
             y = en_output[idx]
-            l = cn[idx, 1:].numpy()
-            for j in range(SEQLEN-1):
+            l = cn_origin[idx, 1:].numpy()
+            for j in range(SEQLEN - 1):
                 if l[j] == data.cntok_end:
                     l[j:] = 0
-            for j in range(SEQLEN-1):
+            for j in range(SEQLEN - 1):
                 if x[j] == data.entok_end:
                     x[j:] = 0
-            for j in range(SEQLEN-1):
+            for j in range(SEQLEN - 1):
                 if y[j] == data.entok_end:
                     y[j:] = 0
-            print(data.cntok.decode(l))
-            print(data.entok.decode(x))
-            print(data.entok.decode(y))
-            print()
-        break
+            x = data.entok.decode(x)
+            xs = word_tokenize(x)
+            y = data.entok.decode(y)
+            ys = word_tokenize(y)
+            b = nltk.translate.bleu_score.sentence_bleu([xs], ys)
+            score_r = rouge.get_scores(y, x)[0]['rouge-l']['f']
+            score_m = nltk.translate.meteor_score.meteor_score([x], y)
+            totalm += score_m
+            totalr += score_r
+            totalb += b
+            countb += 1
+            
+            # print(data.cntok.decode(l))
+            # print(x)
+            # print(y)
+            # print(b)
+            # print()
+        # break
 
     print(count / total)
+    print(totalb / countb)
+    print(totalr / countb)
+    print(totalm / countb)
 
 
 if __name__ == "__main__":
     os.environ["CUDA_VISIBLE_DEVICES"] = "7"
 
-    gpus = tf.config.experimental.list_physical_devices('GPU')
+    gpus = tf.config.experimental.list_physical_devices("GPU")
     if gpus:
         try:
             for gpu in gpus:
                 tf.config.experimental.set_memory_growth(gpu, True)
-                logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+                logical_gpus = tf.config.experimental.list_logical_devices("GPU")
                 print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
         except RuntimeError as e:
             print(e)
